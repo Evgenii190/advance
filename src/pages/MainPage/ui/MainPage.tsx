cec71e34d0acc35ccfbcfd925f1a856c72e62f4c
Evgenii190 <69620869+Evgenii190@users.noreply.github.com>
2025-02-08 00:30:11 +0500

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
     const { t } = useTranslation("main");
-
-    useEffect(() => {
-        throw new Error("123");
-    }, []);
 
     return <div>{t("Главная страница")}</div>;
 };
