07bc131823df61199b23637cfaabaa72aeaf2ec5
Evgenii190 <69620869+Evgenii190@users.noreply.github.com>
2025-03-01 14:09:15 +0500

    fix

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -2,15 +2,15 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ArticleViewSelector } from "./ArticleViewSelector";
 
 export default {
-	title: "shared/ArticleViewSelector",
-	component: ArticleViewSelector,
-	argTypes: {
-		backgroundColor: { control: "color" },
-	},
+    title: "shared/ArticleViewSelector",
+    component: ArticleViewSelector,
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
 } as ComponentMeta<typeof ArticleViewSelector>;
 
 const Template: ComponentStory<typeof ArticleViewSelector> = (args) => {
-	return <ArticleViewSelector {...args} />;
+    return <ArticleViewSelector {...args} />;
 };
 
 export const primary = Template.bind({});
